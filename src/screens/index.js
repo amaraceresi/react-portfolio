45cb7f6f58e01daeaa56fc507b3d96e86487ebf3
Amara Ceresi <aceresi95@gmail.com>
2023-07-19 17:55:30 -0500

    Updates

--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from "./Navbar/Navbar";
-import AboutMe from "./AboutMe/AboutMe";
-import Portfolio from "./Portfolio/Portfolio";
-import Technologies from "./Resume/Resume";
-import Contact from "./Contact/Contact";
-
-export { Navbar, AboutMe, Portfolio, Resume, Contact };
