925b47bbcb56d4e3455a459cef12718e48dd8128
amaraceresi <124629588+amaraceresi@users.noreply.github.com>
2023-07-20 11:18:39 -0500

    Update Contact.jsx

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,18 +1,3 @@
-// import "./Contact.css";
-
-// function Contact() {
-//   return (
-//     <div id="contact" className="contact-container">
-//       <p>Amara Ceresi</p>
-//       <p>Location</p>
-//       <p>+1 (555) 555-5555</p>
-//       <p>email@email.com</p>
-//     </div>
-//   )
-// }
-
-// export default Contact
-
 import "./Contact.css";
 import "./Contact.css";
 import React, { useState } from "react";
